8c9da40c1d9fcb68ec0068b394294aa92dc41053
Max Hvesser <maxhvesser@gmail.com>
2020-01-06 22:03:21 +0100

    Setup view insets for the home fragment.

--- app/src/main/java/no/mhl/clarence/ui/home/HomeFragment.kt
+++ app/src/main/java/no/mhl/clarence/ui/home/HomeFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import no.mhl.clarence.R
+import no.mhl.clarence.ui.views.keypad.KeypadView
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
@@ -26,11 +30,24 @@ class HomeFragment : Fragment() {
 
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ViewCompat.requestApplyInsets(view)
+    }
     // endregion
 
     // region View Setup
     private fun setupView(view: View) {
+        setupViewInsets(view.findViewById(R.id.home_keypad_parent))
+    }
 
+    private fun setupViewInsets(keypadParent: View) {
+        ViewCompat.setOnApplyWindowInsetsListener(keypadParent) { v, insets ->
+            v.updatePadding(bottom = insets.systemWindowInsetBottom)
+            insets
+        }
     }
     // endregion
 
