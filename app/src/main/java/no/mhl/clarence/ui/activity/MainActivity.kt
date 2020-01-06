706e61cc9ce62eb36aa22965c1239aa4227bce6c
Max Hvesser <maxhvesser@gmail.com>
2020-01-06 21:43:51 +0100

    Correctly setup edge to edge view in the main activity.

--- app/src/main/java/no/mhl/clarence/ui/activity/MainActivity.kt
+++ app/src/main/java/no/mhl/clarence/ui/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package no.mhl.clarence.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.findNavController
 import no.mhl.clarence.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -12,10 +14,23 @@ class MainActivity : AppCompatActivity() {
     private val mainViewModel: MainViewModel by viewModel()
     // endregion
 
+    // region View Properties
+    private val viewParent by lazy { findViewById<ConstraintLayout>(R.id.main_parent) }
+    // endregion
+
     // region Initialisation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEdgeToEdge()
+    }
+    // endregion
+
+    // region View Setup
+    private fun setupEdgeToEdge() {
+        viewParent.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
     }
     // endregion
 
